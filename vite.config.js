1cbe503994215532275a3c2b72cb42aeaac6a8ac
Duvvu Lakshmi Prasanna <prasannayadav2468@gmail.com>
2026-04-09 15:48:01 +0530

    fix pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
-import { env } from 'node:process'
-
-const repository = env.GITHUB_REPOSITORY?.split('/')[1] || ''
-const isUserOrOrgPage = repository.endsWith('.github.io')
-const base =
-  env.GITHUB_ACTIONS === 'true' && repository && !isUserOrOrgPage
-    ? `/${repository}/`
-    : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base,
+  base: './',
   plugins: [
     react(),
     visualizer({
